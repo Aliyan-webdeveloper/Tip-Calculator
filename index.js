52bea5608cd8469665727ddad235a591fd111949
aliyan-developer <aliyan500600@gmail.com>
2020-11-06 18:57:23 +0500

    added js file

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-let slider = document.getElementById("rangebar");
 let rangevalue = document.getElementById("rangevalue");
 
-rangevalue.innerHTML = slider.value + "%";
+//input
+let a = document.getElementById('amount-in')
+let b = document.getElementById('rangebar')
+let c = document.getElementById('splitbar')
 
-slider.oninput = function(){
-    rangevalue.innerHTML = this.value + "%";
+//TODO
+rangevalue.innerHTML = b.value + "%";
+splitvalue.innerHTML = c.value;
+
+a.oninput = tipsCalculate
+b.oninput = tipsCalculate
+c.oninput = tipsCalculate
+
+function tipsCalculate() {
+    // console.log('this:',this)
+    let amount = document.getElementById('amount-in').value;
+    let tipPercentage = document.getElementById('rangebar').value;
+    let person = document.getElementById('splitbar').value;
+    let tipper;
+    let totalBill;
+    let aoumntPerPerson;
+    let tipPerPerson;
+    //Formula
+    tipper = Math.floor(amount * (tipPercentage / 100));
+    totalBill = +tipper + +amount;
+    aoumntPerPerson = Math.floor(totalBill / person);
+    tipPerPerson =Math.floor(tipper /  person);
+
+
+    console.log('tipper',tipper)
+
+    //Apppend in document
+    document.getElementById("tipvalue").innerHTML = tipper;
+    document.getElementById("total1").innerHTML = totalBill;
+    document.getElementById("ap-person1").innerHTML = aoumntPerPerson;
+    document.getElementById("tp-person1").innerHTML = tipPerPerson;
+
+
+    rangevalue.innerHTML = tipPercentage + "%";
+    splitvalue.innerHTML = person;
 }
